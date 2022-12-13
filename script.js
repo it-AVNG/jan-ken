24856391016bb2b1fbf127ece127af27d79a2e92
it-AVNG <it.avng@gmail.com>
2022-12-13 22:40:46 +0700

    finish adding playing using button feature

--- script.js
+++ script.js
@@ -1,88 +1,82 @@
-//REQUIRE:
-//create a rock paper scissor game
-//the program take 1 user input that match 
-//either 'rock','paper','scissor'
-//the program calls a computer choice of the same selection
-// the program compare the results with Rock>Scissor>Paper>Rock
-//get bot choice
 var hand =['rock','paper','scissors'];
-
 //PLAN:
+//create function to announce hand
+function updateStat(user,bot){
+  document.getElementById('update').innerText='';
+  document.getElementById('update').innerText=`user play ${user} bot play ${bot}`;
+}
+//create funtion to update winner of the round
+function updateWinner(string){
+  
+  document.getElementById("annouce").innerHTML = '';
+  document.getElementById("annouce").innerHTML += string;
+}
 // create a Function to compare the choices
-function compare(u,b){
-  switch (u) {
+function compare(user,bot){
+  switch (user) {
     case "rock": 
-      console.log(`user play rock bot play ${b}`);
-      if (b == u) {
-        console.log('it is a tie');
+      
+      updateStat(user,bot);
+      if (bot == user) {
+        updateWinner('it is a tie');
         return 'tie';
-      } else if (b == 'paper') {
-        console.log ('bot wins');
+      } else if (bot == 'paper') {
+        updateWinner ('bot wins');
         return 'bot';
       } else {
-        console.log('user wins');
+        updateWinner('user wins');
         return 'user';
       }
       
     case "paper": 
-      console.log (`user play paper bot play ${b}`);
-      if (b == u) {
-        console.log ('it is a tie');
+      updateStat(user,bot);
+      if (bot == user) {
+        updateWinner('it is a tie');
         return 'tie';
-      } else if (b == 'scissors') {
-        console.log ('bot wins');
+      } else if (bot == 'scissors') {
+        updateWinner ('bot wins');
         return 'bot';
       } else {
-        console.log('user wins');
+        updateWinner('user wins');
         return 'user';
       }
       return;
     case "scissors": 
-      console.log (`user play scissors bot play ${b}`);
-      if (b == u) {
-        console.log ('it is a tie');
+      updateStat(user,bot);
+      if (bot == user) {
+        updateWinner('it is a tie');
         return 'tie';
-      } else if (b == 'rock') {
-        console.log ('bot wins');
+      } else if (bot == 'rock') {
+        updateWinner ('bot wins');
         return 'bot';
       } else {
-        console.log('user wins');
+        updateWinner('user wins');
         return 'user';
       }
     
   }
 
 }
-
-//Create a Function to get computer choice 
-function getRndInteger(min, max) {
-  return Math.floor(Math.random() * (max - min) ) + min;
-}
-
-//ramdomly between 'Rock', 'Paper', 'Scissors'
+//Create a Function to get computer random choice 
 function getComputer(array){
   //random select a number between 0 -2
-  let i = getRndInteger(0,3);
+  let i =Math.floor(Math.random() * (3 - 0) ) + 0; ;
   let choice = array[i]; 
   return choice;
 }
-
-//trigger the game
+//trigger the bot selection
 function getbot(){
   var botChoice = getComputer(hand);
-  console.log(botChoice)
-  var winner = compare(userChoice,botChoice);
+  var winner = compare(playerSelection,botChoice);
 }
 
-//create userchoice global variable
-var userChoice
+//create playerSelection global variable
+var playerSelection
 //Delegate evenet listener to the document hand-container
-const handChoice = document.querySelector("#hand-container");
-handChoice.addEventListener("click",
+const playRound = document.querySelector("#hand-container");
+playRound.addEventListener("click",
 function(e){
-  
-  userChoice= `` + e.target.getAttribute('id');
-  console.log(userChoice)
+  playerSelection= `` + e.target.getAttribute('id');
   getbot();
 });
 
